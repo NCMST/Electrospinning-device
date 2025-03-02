0a77c88b81a01e4ecf5eb8b0c67721df7bf9a735
Catalin Creciunel <creciunelcatalin@gmail.com>
2025-03-02 18:32:49 +0200

    Update build file

---
+++ Software/Source_code/buildPlatformIO.sh
@@ -0,0 +1,4 @@
+# pio run
+
+cp -f .pio/build/megaatmega2560/firmware.hex ~/Workspace/Electrospinning-device/Software/Source_code/build
+cp -f .pio/build/megaatmega2560/firmware.elf ~/Workspace/Electrospinning-device/Software/Source_code/build
